73f363bbfe3641f914dc58c74b3e82b3b2796a16
Renan <renan23eduardo@gmail.com>
2023-07-13 18:02:39 -0300

    Create login screen

--- src/View/Login.java
+++ src/View/Login.java
@@ -8,10 +8,17 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -34,21 +41,79 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 792, 720);
+		setBounds(100, 100, 471, 346);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(0, 11, 766, 657);
-		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/View/imagens/Logo.jpg")));
-		contentPane.add(lblNewLabel);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(0, 0, 209, 307);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon(Login.class.getResource("/View/imagens/painel-login.png")));
-		lblNewLabel_1.setBounds(20, 319, 766, 200);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblCenterHospital = new JLabel("CENTER HOSPITAL");
+		lblCenterHospital.setBounds(10, 107, 189, 37);
+		lblCenterHospital.setForeground(Color.WHITE);
+		lblCenterHospital.setFont(new Font("Century Gothic", Font.BOLD, 22));
+		panel.add(lblCenterHospital);
+		
+		JLabel lblNiceTreatment = new JLabel("NICE TREATMENT");
+		lblNiceTreatment.setForeground(Color.WHITE);
+		lblNiceTreatment.setFont(new Font("Century Gothic", Font.BOLD, 22));
+		lblNiceTreatment.setBounds(10, 139, 189, 37);
+		panel.add(lblNiceTreatment);
+		
+		JLabel lblQualifiedStaff = new JLabel("QUALIFIED STAFF");
+		lblQualifiedStaff.setForeground(Color.WHITE);
+		lblQualifiedStaff.setFont(new Font("Century Gothic", Font.BOLD, 22));
+		lblQualifiedStaff.setBounds(10, 173, 189, 37);
+		panel.add(lblQualifiedStaff);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(208, 0, 247, 307);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setBounds(10, 136, 97, 31);
+		lblUsername.setForeground(Color.BLACK);
+		lblUsername.setFont(new Font("Century Gothic", Font.BOLD, 18));
+		panel_1.add(lblUsername);
+		
+		JLabel lblHospital = new JLabel("HMS");
+		lblHospital.setBounds(114, 11, 50, 31);
+		lblHospital.setForeground(Color.BLACK);
+		lblHospital.setFont(new Font("Century Gothic", Font.BOLD, 24));
+		panel_1.add(lblHospital);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setForeground(Color.BLACK);
+		lblPassword.setFont(new Font("Century Gothic", Font.BOLD, 18));
+		lblPassword.setBounds(10, 178, 97, 31);
+		panel_1.add(lblPassword);
+		
+		textField = new JTextField();
+		textField.setBounds(106, 136, 120, 29);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(106, 178, 120, 32);
+		panel_1.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(137, 246, 89, 23);
+		panel_1.add(btnNewButton);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(18, 246, 89, 23);
+		panel_1.add(btnClear);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\renan\\OneDrive\\Ambiente de Trabalho\\cruz.png"));
+		lblNewLabel.setBounds(80, 42, 120, 82);
+		panel_1.add(lblNewLabel);
 	}
-
 }
--- src/View/Splash.java
+++ src/View/Splash.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Label;
+import javax.swing.SwingConstants;
+import javax.swing.JProgressBar;
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
 
 public class Splash extends JFrame {
 
@@ -32,11 +41,39 @@ public class Splash extends JFrame {
 	 */
 	public Splash() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 534, 317);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(5, 5, 508, 268);
+		panel.setBackground(Color.BLACK);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBackground(Color.BLACK);
+		lblNewLabel_1.setForeground(Color.BLACK);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\renan\\OneDrive\\Ambiente de Trabalho\\cruz.png"));
+		lblNewLabel_1.setBounds(186, 87, 128, 93);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("HOSPITAL MANAGEMENT SYSTEM");
+		lblNewLabel.setBounds(59, 0, 374, 31);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Century Gothic", Font.BOLD, 24));
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("%");
+		lblNewLabel_2.setFont(new Font("Century Gothic", Font.BOLD, 24));
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setBounds(237, 208, 28, 31);
+		panel.add(lblNewLabel_2);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setBounds(0, 264, 513, 14);
+		contentPane.add(progressBar);
 	}
-
 }
